2913dfddc823206c180c1d62a164eac7e7874234
가은 <rkdms8228@naver.com>
2022-07-05 16:52:59 +0900

    페이징

--- src/main/java/com/javaex/dao/BoardDao.java
+++ src/main/java/com/javaex/dao/BoardDao.java
@@ -1,5 +1,6 @@
 package com.javaex.dao;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +23,36 @@ public class BoardDao {
 	
 	
 	//메소드-일반
+	//리스트 불러오기 + 검색(페이징)
+	public List<BoardVo> getList2(int startRnum, int endRnum) {
+		
+		System.out.println("BoardDao > getList2");
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("startRnum", startRnum);
+		map.put("endRnum", endRnum);
+		
+		List<BoardVo> boardList = sqlSession.selectList("board.getList2", map);
+		
+		System.out.println(boardList);
+		
+		return boardList;
+		
+	}
+	
+	//전체 글갯수
+	public int selectTotalCnt() {
+		
+		System.out.println("BoardDao > selectTotalCnt");
+	      
+		int totalCount = sqlSession.selectOne("board.selectTotalCnt");
+		
+		System.out.println(totalCount);
+	      
+		return totalCount;
+	      
+	}
+	
 	//리스트 불러오기 + 검색
 	public List<Map<String, Object>> getList(String keyword) {
 		
